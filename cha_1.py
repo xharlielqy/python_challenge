956912f27962aa6f52e7b83cc4732ae93654502c
xharlielqy <charlieli0518@gmail>
2019-03-26 22:32:56 +0000

    use maketrans() to solve out

--- cha_1.py
+++ cha_1.py
@@ -1,15 +1,14 @@
 # python challenge 2: Map
-text = "g fmnc wms bgblr rpylqjyrc gr zw fylb."
-+ "rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb"
-+ "gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle."
-+ " sqgle qrpgle.kyicrpylq() gq pcamkkclbcb."
-+" lmu ynnjw ml rfc spj."
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb." + \
+    " rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb" + \
+    " sqgle qrpgle.kyicrpylq() gq pcamkkclbcb." + \
+    " lmu ynnjw ml rfc spj."
 text_2 = 'map'
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
             'y', 'z']
 new_text = ''
-for letter in text_2:
+for letter in text:
     if letter in alphabet:
         new_letter_position = alphabet.index(letter) + 2
         if new_letter_position > 25:
@@ -23,4 +22,8 @@ for letter in text_2:
         new_text = new_text + letter
 
 print(new_text)
-#  the answer is ocr
+#  try string.maketrans()
+text_3 = 'abcedfghijklmnopqrstuvwxyz'
+text_4 = 'cdefghijklmnopqrstuvwxyzab'
+table = ''.maketrans(text_3, text_4)
+print(text.translate(table))
